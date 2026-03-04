0f4d364b1f4979cc0f49562d4bad49951631e263
Akash-srm861 <akash@example.com>
2026-03-04 15:40:04 +0530

    Base application setup

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,7 @@
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+        // Palindrome Checker Application
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# PalindromeCheckerApp
+
+
+//running procedure
+//flow of project
+//objective
+
+//working of branch 2
