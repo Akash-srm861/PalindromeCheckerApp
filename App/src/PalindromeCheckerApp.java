5252177c18878029ae3f05d69460c70dc82ed97b
Akash <akashrgs212006@gmail.com>
2026-03-05 09:32:04 +0530

    Remove welcome message from PalindromeCheckerApp
    Removed welcome message from the main method.

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -2,11 +2,7 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        // UC1: Welcome Message
-        System.out.println("=================================");
-        System.out.println("  Welcome to Palindrome Checker  ");
-        System.out.println("=================================");
-
+   
         // UC2: Hardcoded Palindrome Check
         String word = "racecar";
         boolean isPalindrome = true;
